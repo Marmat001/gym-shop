822bad9ee2cfb3d25e01d15f1c46cbbc03ffea4c
marmat001 <markusmatu@Markuss-MacBook-Pro.local>
2020-11-09 20:54:54 +0100

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout"
+
+const StripeCheckoutButton = ({price}) => {
+    const priceForStripe = price * 100;
+    const publishableKey = "pk_test_51Hlfr9AqrkYgU5i24jc4cLdcOjqz2aQH27TNdJ8897x6rlGYx5prlE53sURsSa7ACtF4o6emWgETHo0P36h6Fzxt00GE1DBk2Q"
+
+   const onToken = token => {
+        console.log(token)
+        alert("Payment Successful")
+    }
+
+    return (
+        <StripeCheckout 
+        label="Pay Now"
+        name ="XXDX Clothing Ltd."
+        billingAddress
+        shippingAddress
+        image="https://sendeyo.com/up/d/f3eb2117da"
+        description={`Your total is $${price}`}
+        amount={priceForStripe}
+        panelLabel="Pay Now"
+        token={onToken}
+        stripeKey={publishableKey}
+         />
+    )
+}
+
+export default StripeCheckoutButton
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import {createStructuredSelector} from "reselect"
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors"
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component"
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component"
 
 import "./checkout.style.scss"
 
@@ -34,9 +35,14 @@ const CheckOutPage = ({cartItems, total}) => (
         ))
     }
     
-        <div className="total">
-            <span>TOTAL: ${total}</span>
+        <div className="total">TOTAL: ${total}</div>
+        <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
         </div>
+
+        <StripeCheckoutButton price={total} />
     </div>
 )
 
